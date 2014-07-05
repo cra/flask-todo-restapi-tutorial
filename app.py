0310dcc1e1591c0bec934d7ab92bdd2202cb8072
Igor Mosyagin <bernd@cramur.me>
2014-07-05 23:47:07 +0200

    Refactor a bit

--- app.py
+++ app.py
@@ -40,63 +40,53 @@ def not_found(error):
     return make_response(jsonify({'error': "Not found"}), 404)
 
 
-@app.route('/api/v1.0/tasks/', methods=["GET"])
-def get_tasks():
-    return jsonify({'tasks': map(make_public_task, tasks)})
+@app.route('/api/v1.0/tasks/', methods=["GET", "POST"])
+def tasks_index():
+    if request.method == "GET":
+        return jsonify({'tasks': map(make_public_task, tasks)})
+    if request.method == "POST":
+        if not request.json or not 'title' in request.json:
+            abort(400)
+        task = {
+            'id': tasks[-1]['id'] + 1,
+            'title': request.json['title'],
+            'description': request.json.get('description', ""),
+            'done': False,
+        }
+        tasks.append(task)
+        return jsonify({'task': task}), 201
 
 
-@app.route('/api/v1.0/tasks/', methods=["POST"])
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False,
-    }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
+ACCESS_URL = "/api/v1.0/tasks/<int:task_id>"
 
-
-@app.route('/api/v1.0/tasks/<int:task_id>', methods=["GET"])
+@app.route(ACCESS_URL, methods=["GET", "PUT", "DELETE"])
 def get_task(task_id):
     task = filter(lambda t: t['id'] == task_id, tasks)
     if len(task) == 0:
         abort(404)
-    return jsonify({'task': task[0] })
 
-
-@app.route('/api/v1.0/tasks/<int:task_id>', methods=["PUT"])
-def update_task(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
-    if len(task) == 0:
-        abort(404)
-    if not request.json:
-        abort(400)
-
-    fields_with_types = [('title', unicode), ('description', unicode), ('done', bool)]
-
-    t = bool
-    for f, t in fields_with_types:
-        if f in request.json and type(request.json[f]) is not t:
+    if request.method == "GET":
+        return jsonify({'task': task[0] })
+    elif request.method == "PUT":
+        if not request.json:
             abort(400)
+        fields_with_types = [
+                ('title', unicode),
+                ('description', unicode),
+                ('done', bool)
+            ]
 
-    for f, _ in fields_with_types:
-        task[0][f] = request.json.get(f, task[0][f])
+        for f, t in fields_with_types:
+            if f in request.json and type(request.json[f]) is not t:
+                abort(400)
 
-    return jsonify({'task': task[0] })
+        for f, _ in fields_with_types:
+            task[0][f] = request.json.get(f, task[0][f])
 
-
-@app.route('/api/v1.0/tasks/<int:task_id>', methods=["DELETE"])
-def delete_task(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
-    
-    if len(task) == 0:
-        abort(404)
-
-    tasks.remove(task[0])
-    return jsonify({'result': True})
+        return jsonify({'task': task[0] })
+    elif request.method == "DELETE":
+        tasks.remove(task[0])
+        return jsonify({'result': True})
 
 
 if __name__ == "__main__":
